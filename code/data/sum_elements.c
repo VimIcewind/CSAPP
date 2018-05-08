9670f6476a83137e770a1dd5a544aa98273e5e34
VimIcewind <VimIcewind@gmail.com>
2018-05-08 16:56:26 +0800

    Fix bug

--- code/data/sum_elements.c
+++ code/data/sum_elements.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/* WARNING: This is buggy code */
 float sum_elements(float a[], unsigned length)
 {
 	int i;
 	float result = 0;
 
-	for (i = 0; i <= length-1; i++)
+	for (i = 0; i < length; i++)
 		result += a[i];
 	return result;
 }
