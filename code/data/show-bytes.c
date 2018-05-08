522d8a7b7fba49ee38de0a5aca3fe00d1c29f75b
VimIcewind <VimIcewind@gmail.com>
2018-05-08 15:26:10 +0800

    Expanding the Bit Representation of a Number

--- code/data/show-bytes.c
+++ code/data/show-bytes.c
@@ -45,9 +45,27 @@ void test_twoscomplement()
 	show_bytes((byte_pointer) &mx, sizeof(short));
 }
 
+void test_expand()
+{
+	short sx = -12345;	/* -12345 */
+	unsigned short usx = sx;/*  53191 */
+	int x = sx;		/* -12345 */
+	unsigned ux = usx;	/*  53191 */
+
+	printf("sx = %d:\t", sx);
+	show_bytes((byte_pointer) &sx, sizeof(short));
+	printf("usx = %u:\t", usx);
+	show_bytes((byte_pointer) &usx, sizeof(unsigned short));
+	printf("x = %d:\t", x);
+	show_bytes((byte_pointer) &x, sizeof(int));
+	printf("ux = %u:\t", ux);
+	show_bytes((byte_pointer) &ux, sizeof(unsigned));
+}
+
 int main(int argc, const char *argv[])
 {
 	test_show_bytes(12345);
 	test_twoscomplement();
+	test_expand();
 	return 0;
 }
