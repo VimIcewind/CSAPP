834dbcacb5c08ceaa0d10b4a3d4c53558bb90042
VimIcewind <VimIcewind@gmail.com>
2018-05-08 17:16:26 +0800

    strlonger bug version

---
+++ code/data/strlonger.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Determine whether string s is longer than string t */
+/* WARNING: This function is buggy */
+int strlonger(char *s, char *t) {
+	return strlen(s) - strlen(t) > 0;
+}
+
+void test_strlonger()
+{
+	char *s0 = "Hi";
+	char *s1 = "Hello";
+	char *s2 = "World";
+
+	printf("%d\n", strlonger(s0, s1));
+	printf("%d\n", strlonger(s1, s0));
+	printf("%d\n", strlonger(s1, s2));
+
+}
+
+int main(int argc, const char *argv[])
+{
+	test_strlonger();
+	
+	return 0;
+}
