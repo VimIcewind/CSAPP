834439531709461b9ab02e172b5cc3edec3c99ca
VimIcewind <VimIcewind@gmail.com>
2020-01-02 10:08:46 +0800

    asm

---
+++ code/asm/exchangedemo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+long exchange(long *xp, long y);
+
+int main(int argc, const char *argv[])
+{
+	long a = 4;
+	long b = exchange(&a, 3);
+	printf("a = %ld, b = %ld\n", a, b);
+	return 0;
+}
