aac4fdcbc7b6e1aa3da2a3681acc1425f062bea3
VimIcewind <VimIcewind@gmail.com>
2020-01-28 11:39:01 +0800

    Just use make to generate object file

--- code/asm/exchangedemo.c
+++ code/asm/exchangedemo.c
@@ -4,8 +4,12 @@ long exchange(long *xp, long y);
 
 int main(int argc, const char *argv[])
 {
-	long a = 4;
-	long b = exchange(&a, 3);
+	long a = 4, b = 3;
+
+	printf("Before exchange:\n");
+	printf("a = %ld, b = %ld\n", a, b);
+	b = exchange(&a, 3);
+	printf("After exchange:\n");
 	printf("a = %ld, b = %ld\n", a, b);
 	return 0;
 }
