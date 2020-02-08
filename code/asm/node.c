dbcfc540e4e622bbd07b090e068411e56655b35c
VimIcewind <VimIcewind@gmail.com>
2020-02-08 17:54:42 +0800

    node_t

--- code/asm/node.c
+++ code/asm/node.c
@@ -14,9 +14,22 @@ union node_u {
 	double data[2];
 };
 
+typedef enum {N_LEAF, N_INTERNAL} nodetype_t;
+
+struct node_t {
+	nodetype_t type;
+	union {
+		struct {
+			struct node_t *left;
+			struct node_t *right;
+		} internal;
+	} info;
+};
+
 int main(int argc, char *argv[])
 {
 	printf("sizeof(struct node_s) is %ld\n", sizeof(struct node_s));
 	printf("sizeof(union node_u) is %ld\n", sizeof(union node_u));
+	printf("sizeof(struct node_t) is %ld\n", sizeof(struct node_t));
 	return 0;
 }
