91f0bf9649164e973c63cdded7517bb2845650ca
VimIcewind <VimIcewind@gmail.com>
2021-01-13 11:21:24 +0800

    forkfork

--- code/ecf/forkfork.c
+++ code/ecf/forkfork.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-void unix_error(char *msg) /* Unix-style error */
+void unix_error(char *msg)
 {
 	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 	exit(0);
@@ -22,16 +22,8 @@ pid_t Fork(void)
 
 int main()
 {
-	pid_t pid;
-	int x = 1;
-
-	pid = Fork();
-	if (pid == 0) { /* Child */
-		printf("child : x=%d\n", ++x);
-		exit(0);
-	}
-
-	/* Parent */
-	printf("parent: x=%d\n", --x);
+	Fork();
+	Fork();
+	printf("hello\n");
 	exit(0);
 }
