89f079785f23f51f6cf82a5f74453fccfe43bfb3
VimIcewind <VimIcewind@gmail.com>
2021-01-24 15:04:03 +0800

    sigintsafe

---
+++ code/ecf/sigintsafe.c
@@ -0,0 +1,18 @@
+#include "csapp.h"
+
+void sigint_handler(int sig) /* Safe SIGINT handler */
+{
+	Sio_puts("Caught SIGINT!\n"); /* Safe output */
+	_exit(0); /* Safe exit */
+}
+
+int main(void)
+{
+	/* Install the SIGINT handler */
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		unix_error("signal error");
+
+	pause(); /* Wait for the receipt of a signal */
+
+	return 0;
+}
