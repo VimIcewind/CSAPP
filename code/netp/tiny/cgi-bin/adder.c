669eba5efbeaf5cc1271fb35aebdee44f8164aa6
VimIcewind <VimIcewind@gmail.com>
2021-03-25 15:50:00 +0800

    Fix bug in adder

--- code/netp/tiny/cgi-bin/adder.c
+++ code/netp/tiny/cgi-bin/adder.c
@@ -7,7 +7,7 @@ int main(void)
 	int n1 = 0, n2 = 0;
 
 	/* Extract the two arguments */
-	if ((buf = getenv("QUARY_STRING")) != NULL) {
+	if ((buf = getenv("QUERY_STRING")) != NULL) {
 		p = strchr(buf, '&');
 		*p = '\0';
 		strcpy(arg1, buf);
@@ -17,7 +17,7 @@ int main(void)
 	}
 
 	/* Make the response body */
-	sprintf(content, "QUARY_STRING=%s", buf);
+	sprintf(content, "QUERY_STRING=%s", buf);
 	sprintf(content, "Welcome to add.com: ");
 	sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
 	sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
