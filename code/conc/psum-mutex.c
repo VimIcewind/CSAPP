d6cd4b8885a0bdaf9a281f3dc7cbd3089b56b0d9
VimIcewind <VimIcewind@gmail.com>
2021-04-16 22:37:00 +0800

    psum-mutex

---
+++ code/conc/psum-mutex.c
@@ -0,0 +1,56 @@
+#include "csapp.h"
+
+#define MAXTHREADS 32
+
+void *sum_mutex(void *vargp); /* Thread toutine */
+
+/* Global shared variables */
+long gsum = 0;		/* Global sum */
+long nelems_per_thread; /* Number of elements to sum */
+sem_t mutex;		/* Mutex to protect global sum */
+
+int main(int argc, char **argv)
+{
+	long i, nelems, log_nelems, nthreads, myid[MAXTHREADS];
+	pthread_t tid[MAXTHREADS];
+
+	/* Get input arguments */
+	if (argc != 3) {
+		printf("Usage: %s <nthreads> <log_nelems>\n", argv[0]);
+		exit(0);
+	}
+	nthreads = atoi(argv[1]);
+	log_nelems = atoi(argv[2]);
+	nelems = (1L << log_nelems);
+	nelems_per_thread = nelems / nthreads;
+	sem_init(&mutex, 0, 1);
+
+	/* create peer threads and wait for them to finish */
+	for (i = 0; i < nthreads; i++) {
+		myid[i] = i;
+		Pthread_create(&tid[i], NULL, sum_mutex, &myid[i]);
+	}
+	for (i = 0; i < nthreads; i++)
+		Pthread_join(tid[i], NULL);
+
+	/* Check final answer */
+	if (gsum != (nelems * (nelems-1))/2)
+		printf("Error: result=%ld\n", gsum);
+	exit(0);
+}
+
+/* Thread routine for psum-mutex.c */
+void *sum_mutex(void *vargp)
+{
+	long myid = *((long *)vargp);		/* Extract the thread ID */
+	long start = myid * nelems_per_thread;	/* Start element index */
+	long end = start + nelems_per_thread;	/* End element index */
+	long i;
+
+	for (i = start; i < end; i++) {
+		P(&mutex);
+		gsum += i;
+		V(&mutex);
+	}
+	return NULL;
+}
