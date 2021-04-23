058a291ef813cc0e64a9cf46103120fb419a2c8a
VimIcewind <VimIcewind@gmail.com>
2021-04-23 11:36:36 +0800

    norace-val

---
+++ code/conc/norace-val.c
@@ -0,0 +1,25 @@
+#include "csapp.h"
+#define N 4
+
+void *thread(void *vargp);
+
+int main()
+{
+	pthread_t tid[N];
+	int i, *ptr;
+
+	for (i = 0; i < N; i++) {
+		Pthread_create(&tid[i], NULL, thread, (void *)i);
+	}
+	for (i = 0; i < N; i++)
+		Pthread_join(tid[i], NULL);
+	exit(0);
+}
+
+/* Thread routine */
+void *thread(void *vargp)
+{
+	int myid = (int)vargp;
+	printf("Hello from thread %d\n", myid);
+	return NULL;
+}
